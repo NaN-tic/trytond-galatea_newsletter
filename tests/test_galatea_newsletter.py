3a989433cac7ab994a4f3d518ac0ced956ee4258
resteve <resteve@zikzakmedia.com>
2015-11-29 22:25:36 +0100

    Tests

--- tests/test_galatea_newsletter.py
+++ tests/test_galatea_newsletter.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-# This file is part galatea_newsletter module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the galatea_newsletter module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class GalateaNewsletterTestCase(unittest.TestCase):
+class GalateaNewsletterTestCase(ModuleTestCase):
     'Test Galatea Newsletter module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('galatea_newsletter')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'galatea_newsletter'
 
 
 def suite():
